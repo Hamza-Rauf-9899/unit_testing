590e1cf735450cb96a903d6dc5d89f808e77d26c
Hamza-Rauf-9899 <simplekpk724.com>
2022-03-07 14:24:32 +0500

    skipping some tests

--- tests.cpp
+++ tests.cpp
@@ -7,6 +7,7 @@ using namespace std;
 TEST(SquareRootTest, PositiveNos) { 
     ASSERT_EQ(6, squareRoot(36.0));
     ASSERT_EQ(18.0, squareRoot(324.0));
+    GTEST_SKIP() << "Skipping two tests";
     ASSERT_EQ(25.4, squareRoot(645.16));
     ASSERT_EQ(0, squareRoot(0.0));
 }
